a01e4c0418ee24afcd9746af5ee7e3753fe61383
ABDUKODIROV FARKHOD <abdukodirovfarkhod@gmail.com>
2023-06-08 15:02:06 +0900

    Delete InstallerWizard directory

--- InstallerWizard/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.installerwizard {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.installerwizard to javafx.fxml;
-    exports com.example.installerwizard;
-}
